8630e76db2e4d9a78ddf8f5828e08de46afb58a8
MANUELJOSEE <mrodriguezs@fwdcostarica.com>
2023-08-09 15:47:03 -0600

    Hola

--- src/panel.js
+++ src/panel.js
@@ -1,4 +1,35 @@
+class Panel {
+  constructor() {
+    this.diccionario = ["comida", "correr", "jugar", "caminar", "programar"];
+    this.palabraselecionada = "";
+    this.vectorRespuesta = [];
+    this.vectorLineas = [];
+  }
+
+  elegirPalabra() {
+    const posicion = Math.floor(
+      Math.random() * (this.diccionario.length - 0),
+      0
+    );
+
+    this.palabraselecionada = this.diccionario[posicion];
+    console.log(this.palabraselecionada);
+  }
+
+  dibujarLineas() {
+    const lines = Array.from(this.palabraselecionada);
+
+    for (let index = 0; index < lines.length; index++) {
+      this.vectorLineas.push("_");
+    }
+    
 
 
+  }
+}
 
+const app = new Panel();
+app.elegirPalabra();
+
+console.log(app.dibujarLineas());
 
